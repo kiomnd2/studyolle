2d2e025c475ea83a444ce7d9b328491ae85426be
kiomnd2 <kiomnd2@naver.com>
2020-06-27 00:24:58 +0900

    이메일 체크 uuid 생성 메서드

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter @Setter
@@ -39,7 +40,8 @@ public class Account {
 
     private String location;
 
-    @Lob @Basic(fetch = FetchType.EAGER)
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
     private String profileImage;
 
 
@@ -55,4 +57,8 @@ public class Account {
 
     private boolean studyUpdatedByWeb;
 
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+    }
 }
