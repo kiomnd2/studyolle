6be592e8be78023373fb48d14822544770406bd4
kiomnd2 <kiomnd2@naver.com>
2020-07-11 03:18:51 +0900

    스터디 설정 -> 지역 ,태그 설정 추가

--- src/main/java/com/studyolle/settings/StudySettingsController.java
+++ src/main/java/com/studyolle/settings/StudySettingsController.java
@@ -21,6 +21,7 @@ import com.studyolle.study.StudyService;
 import com.studyolle.tag.TagRepository;
 import com.studyolle.zone.ZoneForm;
 import com.studyolle.zone.ZoneRepository;
+import com.studyolle.zone.ZoneService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
@@ -45,6 +46,48 @@ public class StudySettingsController {
 
     private final StudyService studyService;
     private final ModelMapper modelMapper;
+    private final TagRepository tagRepository;
+    private final ObjectMapper objectMapper;
+    private final TagService tagService;
+    private final ZoneRepository zoneRepository;
+    private final ZoneService zoneService;
+
+
+    @GetMapping("/banner")
+    public String studyImageForm(@CurrentUser Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+        return "study/settings/banner";
+    }
+
+
+
+    @PostMapping("/banner")
+    public String studyImageSubmit(@CurrentUser Account account, @PathVariable String path
+            , String image, RedirectAttributes attributes) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.updateStudyImage(study, image);
+        attributes.addFlashAttribute("message", "스터디 이미지를 수정했습니다.");
+        return "redirect:/study/" + getPath(path) + "/settings/banner";
+
+    }
+
+
+    @PostMapping("/banner/enable")
+    public String enableStudyBanner(@CurrentUser Account account, @PathVariable String path) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.enableStudyBanner(study);
+        return "redirect:/study/" + getPath(path) +"/settings/banner";
+    }
+
+
+    @PostMapping("/banner/disable")
+    public String disableStudyBanner(@CurrentUser Account account, @PathVariable String path) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.disableStudyBanner(study);
+        return "redirect:/study/" + getPath(path) +"/settings/banner";
+    }
 
 
 
@@ -73,6 +116,81 @@ public class StudySettingsController {
         attributes.addFlashAttribute("message", "스터디 소개를 수정했습니다.");
         return "redirect:/study/" + getPath(path) + "/settings/description";
     }
+
+    @GetMapping("/tags")
+    public String studyTagsForm(@CurrentUser Account account, @PathVariable String path, Model model) throws Exception {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+
+        model.addAttribute("tags", study.getTags().stream().map(Tag::getTitle).collect(Collectors.toList()));
+        List<String> allTagTitles = tagRepository.findAll().stream().map(Tag::getTitle).collect(Collectors.toList());
+
+        model.addAttribute("whiteList", objectMapper.writeValueAsString(allTagTitles));
+        return "study/settings/tags";
+    }
+
+    @PostMapping("/tags/add")
+    @ResponseBody
+    public ResponseEntity addTag(@CurrentUser Account account, @PathVariable String path, @RequestBody TagForm tagForm) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        Tag tag = tagService.findOrcreateNew(tagForm.getTagTitle());
+        studyService.addTag(study, tag);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/tags/remove")
+    @ResponseBody
+    public ResponseEntity removeTag(@CurrentUser Account account, @PathVariable String path, @RequestBody TagForm tagForm) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        Tag tag = tagRepository.findByTitle(tagForm.getTagTitle());
+        if (tag == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        studyService.removeTag(study, tag);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+    @GetMapping("/zones")
+    public String studyZoneForm(@CurrentUser Account account, @PathVariable String path, Model model) throws Exception {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+
+        model.addAttribute("zone", study.getZones().stream().map(Zone::toString).collect(Collectors.toList()));
+        List<String> allTagTitles = zoneRepository.findAll().stream().map(Zone::toString).collect(Collectors.toList());
+
+        model.addAttribute("whiteList", objectMapper.writeValueAsString(allTagTitles));
+        return "study/settings/zones";
+    }
+
+    @PostMapping("/zones/add")
+    @ResponseBody
+    public ResponseEntity addZone(@CurrentUser Account account, @PathVariable String path, @RequestBody ZoneForm zoneForm) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        Zone zone = zoneRepository.findByCityAndProvince(zoneForm.getCityName(), zoneForm.getProvinceName());
+        studyService.addZone(study, zone);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/zones/remove")
+    @ResponseBody
+    public ResponseEntity removeZone(@CurrentUser Account account, @PathVariable String path, @RequestBody ZoneForm zoneForm) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        Zone zone = zoneRepository.findByCityAndProvince(zoneForm.getCityName(), zoneForm.getProvinceName());
+        if (zone == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        studyService.removeZone(study, zone);
+        return ResponseEntity.ok().build();
+    }
+
 
     private String getPath(String path) {
         return URLEncoder.encode(path, StandardCharsets.UTF_8);
---
+++ src/main/java/com/studyolle/settings/TagService.java
@@ -0,0 +1,22 @@
+package com.studyolle.settings;
+
+import com.studyolle.domain.Tag;
+import com.studyolle.tag.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TagService {
+
+    private final TagRepository tagRepository;
+    public Tag findOrcreateNew(String tagTitle) {
+        Tag tag = tagRepository.findByTitle(tagTitle);
+        if (tag == null) {
+            tag = tagRepository.save(Tag.builder().title(tagTitle).build());
+        }
+        return tag;
+    }
+}
